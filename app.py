3a77048a00c055b8fe802a6e9c87a2cdd95e9029
Brygetee Al-Shawkani <brygetee@gmail.com>
2026-01-05 22:34:24 -0500

    completed first draft of entry form

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from calendar import month
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,7 +10,10 @@ def home():
 
 @app.route("/entry_form")
 def entry_form():
-    return render_template("entry_form.html")
+    subjects = ["subject1","subject2","subject3"]
+    months=["January","February","March","April","May","June","July","August","September","October","November","December"]
+    return render_template("entry_form.html", subjects=subjects, months=months)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=3000
+            )
