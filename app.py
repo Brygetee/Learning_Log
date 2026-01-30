509262ff8bbbec1699c0089b03082774320e530a
Brygetee Al-Shawkani <brygetee@gmail.com>
2026-01-29 20:52:03 -0500

    Enabled Form Submission and first draft success message

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from calendar import month
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,11 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/entry_form")
+@app.route("/entry_form", methods=["POST","GET"])
 def entry_form():
-    subjects = ["subject1","subject2","subject3"]
+    subjects = ["subject1", "subject2", "subject3"]
+
+    if request.method == "POST":
+        title = request.form.get("title")
+        subject = request.form.get("subject")
+        concepts_learned = request.form.get("concepts_learned")
+        reference_notes = request.form.get("reference_notes")
+        key_takeaways = request.form.get("key_takeaways")
+        date = request.form.get("date")
+        return f"Form submitted successfully! Entry Name: {title}, Subject: {subject}"
+
     return render_template("entry_form.html", subjects=subjects)
 
+# def submit():
 if __name__ == "__main__":
-    app.run(debug=True, port=3000
-            )
+    app.run(debug=True, port=3000)
